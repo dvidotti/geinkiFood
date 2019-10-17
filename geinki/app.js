e4e529b7fd454d95003f08c9785aeeec6f56444b
Gabriel Escame <gabriel.escame@gmail.com>
2019-10-17 18:19:33 -0300

    viewHome-Css

--- geinki/app.js
+++ geinki/app.js
@@ -39,11 +39,11 @@ app.use(session({
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
       
 
 app.set('views', path.join(__dirname, 'views'));
